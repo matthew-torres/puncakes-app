5574b058bee1682f243da562eb844db3359ec0b4
matthew-torres <matt@USF-Gold-Wifi-226-63-60.laptops.usf.edu>
2023-03-07 12:41:08 -0500

    Added customer table and insert queries, api routes

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 import db
-from dotenv import load_dotenv
 
 from flask import Flask, render_template, request
 
@@ -10,11 +9,31 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
-@app.route("/employee_signup")
+@app.post("/employee_signup")
 def employee_signup():
     data = request.get_json()
     # TODO: add check box in frontend to determine if manager or staff
-    if (employee_type == "manager"):
+    if (data["employee_type"] == "manager"):
         db.insert_new_manager(data["name"], data["salary"], data["password"], data["startDate"], data["jobTitle"])
     else:
         db.insert_new_staff(data["name"], data["salary"], data["password"], data["startDate"], data["jobTitle"])
+
+@app.post("/customer_signup")
+def customer_signup():
+    data = request.get_json()
+    db.insert_new_customer(data["name"], data["password"], data["email"], data["phoneNum"], data["street"], data["city"], data["state"], data["zip"])
+
+
+app.get("/user/employee/<eid>")
+def get_employee_page():
+    pass
+
+app.get("/user/customer/<cid>")
+def get_customer_page():
+    pass
+
+app.get("/products/")
+def get_product_page():
+    pass
+
+
--- db.py
+++ db.py
@@ -19,21 +19,33 @@ def create_tables():
                     password    VARCHAR(15),
                     startDate   DATE
                 );
-                CREATE TABLE IF NOT EXISTS  manager (
+                CREATE TABLE IF NOT EXISTS manager (
                     eid         SERIAL PRIMARY KEY,
                     jobTitle    VARCHAR(20),
                     FOREIGN KEY (eid) REFERENCES employees(eid)
                 );
 
-                CREATE TABLE IF NOT EXISTS  staff (
+                CREATE TABLE IF NOT EXISTS staff (
                     eid         SERIAL REFERENCES employees,
                     jobTitle    VARCHAR(20),
                     FOREIGN KEY (eid) REFERENCES employees(eid)
                 );
+
+                CREATE TABLE IF NOT EXISTS customers (
+                    cid         SERIAL PRIMARY KEY,
+                    name        VARCHAR(20),
+                    password    VARCHAR(20),
+                    email       VARCHAR(20),
+                    phoneNum    CHAR(10),
+                    street      VARCHAR(20),
+                    city        VARCHAR(15),
+                    state       CHAR(2),
+                    zip         CHAR(5)    
+                );
             """)
             # cursor.execute("""DROP TABLE employees, manager, staff;""")
     
-def insert_new_manager(name, salary, password, startDate):
+def insert_new_manager(name, salary, password, startDate, jobTitle):
     with connection:
         with connection.cursor() as cursor:
             cursor.execute("""
@@ -49,12 +61,11 @@ def insert_new_manager(name, salary, password, startDate):
                     jobTitle    VARCHAR(20),
                     FOREIGN KEY (eid) REFERENCES employees(eid)
                 );
-                f"WITH e as (INSERT INTO employees(name, salary, password, startDate) 
-                VALUES ({name}, {salary}, {password}, {startDate}) 
+                WITH e as (INSERT INTO employees(name, salary, password, startDate) 
+                VALUES (%s, %d, %s, %s)
                 RETURNING eid)
-                INSERT INTO manager(eid, jobTitle) SELECT e.eid, {jobTitle} as jobTitle FROM e;"
-
-            """)
+                INSERT INTO manager(eid, jobTitle) SELECT e.eid, %s  as jobTitle FROM e;
+            """, (name, salary, password, startDate, jobTitle,))
 
 def insert_new_staff(name, salary, password, startDate, jobTitle):
     with connection:
@@ -72,10 +83,30 @@ def insert_new_staff(name, salary, password, startDate, jobTitle):
                     jobTitle    VARCHAR(20),
                     FOREIGN KEY (eid) REFERENCES employees(eid)
                 );
-                f"WITH e as (INSERT INTO employees(name, salary, password, startDate) 
-                VALUES ({name}, {salary}, {password}, {startDate}) 
+                WITH e as (INSERT INTO employees(name, salary, password, startDate) 
+                VALUES (%s, %d, %s, %s) 
                 RETURNING eid)
-                INSERT INTO staff(eid, jobTitle) SELECT e.eid, {jobTitle} as jobTitle FROM e;"
+                INSERT INTO staff(eid, jobTitle) SELECT e.eid, %s as jobTitle FROM e;
+            """, (name, salary, password, startDate, jobTitle,))
 
-            """)
+def insert_new_customer(name, password, email, phone_num, street, city, state, zip):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(f""" 
+                CREATE TABLE IF NOT EXISTS customers (
+                    cid         SERIAL PRIMARY KEY,
+                    name        VARCHAR(20),
+                    password    VARCHAR(20),
+                    email       VARCHAR(20),
+                    phoneNum    CHAR(10),
+                    street      VARCHAR(20),
+                    city        VARCHAR(15),
+                    state       CHAR(2),
+                    zip         CHAR(5)    
+                );
+                INSERT INTO customers(name, password, email, phoneNum, street, city, state, zip) 
+                VALUES(%s, %s, %s, %s, %s, %s, %s, %s);
+            """, (name, password, email, phone_num, street, city, state, zip,))
+
+create_tables()
             
