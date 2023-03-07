7dd961b420c68ca3a5bd0f281b8e4d00ca876c06
matthew-torres <matt@Matts-MacBook-Pro-2.local>
2023-03-06 20:52:48 -0500

    Revisions

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 import os
-import psycopg2, db
+import db
 from dotenv import load_dotenv
 
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-url = os.environ.get("DATABASE_URL")
-connection = psycopg2.connect(url)
 
 @app.route("/")
 def hello_world():
@@ -15,7 +13,7 @@ def hello_world():
 @app.route("/employee_signup")
 def employee_signup():
     data = request.get_json()
-    # TOOD: add check box in frontend to determine if manager or staff
+    # TODO: add check box in frontend to determine if manager or staff
     if (employee_type == "manager"):
         db.insert_new_manager(data["name"], data["salary"], data["password"], data["startDate"], data["jobTitle"])
     else:
