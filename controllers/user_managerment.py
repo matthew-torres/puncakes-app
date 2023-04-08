f6267b51e3e2e8624092cb06dd60d02a405843d1
matthew-torres <matt.torres@gmail.com>
2023-04-08 17:43:23 -0400

    Created login and sessions for employee/customers

---
+++ controllers/user_managerment.py
@@ -0,0 +1,7 @@
+from flask import session
+
+def create_session(user: tuple):
+            session['loggedin'] = True
+            session['id'] = user[0] # index of cid
+            session['username'] = user[1] # index of customer name in tuple
+            return True
