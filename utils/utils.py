d8df9c46a8185e825e64a9551303843daf338d5f
William <gonzalez23.will@gmail.com>
2023-04-27 23:51:23 -0400

    a

--- utils/utils.py
+++ utils/utils.py
@@ -3,7 +3,7 @@ from db import db
 
 def consolidate_cart(cart):
     '''
-    Consolidate indentical items into one cart entry
+    Consolidate identical items into one cart entry
     Params:
         cart: list of dictionaries
     Returns:
@@ -14,13 +14,26 @@ def consolidate_cart(cart):
         found = False
         for i in range(len(result)):
             if result[i]['id'] == d['id']:
-                result[i]['quantity'] += d['quantity']
+                result[i]['quantity'] += int(d['quantity'])
                 found = True
                 break
         if not found:
             result.append(d)
     return result
 
+
+def totalize_cart(cart):
+    '''
+    Totalize the cart cost
+    Params:
+        cart: list of dictionaries
+    Returns:
+        result: cart total
+    '''
+    total = 0
+    for d in cart:
+        total += (int(d['quantity']) * float(d['price']))
+    return total
 
 def update_order_status_completed(orders):
     '''
